7394241ac68a903bf01a687fa2304a87477ee634
vyuv <kaavolv@gmail.com>
2020-04-03 23:31:30 +0300

    desktop

--- src/components/layout/MainMenu.tsx
+++ src/components/layout/MainMenu.tsx
@@ -10,9 +10,10 @@ import styles from './MainMenu.module.css';
 
 interface Props {
   activeTab: PageType;
+  showDesktop: boolean;
 }
 
-const _MainMenu: React.FC<Props> = ({ activeTab }) => {
+const _MainMenu: React.FC<Props> = ({ activeTab, showDesktop }) => {
   const router = useRouter();
   const handleOnClick = (path: string, external: boolean) => {
     if (external) {
@@ -24,26 +25,29 @@ const _MainMenu: React.FC<Props> = ({ activeTab }) => {
 
   return (
     <>
-      <div className={styles.mainMenu}>
-        <img className={`${styles.logo} ${styles.bigLogo}`} src={logoAccent} alt='' />
-        <div className={styles.menuContainer}>
+      {showDesktop ? (
+        <div className={styles.mainMenu}>
+          <img className={`${styles.logo} ${styles.bigLogo}`} src={logoAccent} alt='' />
+          <div className={styles.menuContainer}>
+            {MAINMENU_ITEMS.map(item => (
+              <MainMenuItem key={item.label} item={item} active={activeTab === item.label} handleOnClick={handleOnClick} />
+            ))}
+          </div>
+          <div className={styles.menuGroupLine} />
+          <div className={styles.menuContainer}>
+            {MAINMENU_DESKTOP_ITEMS.map(item => (
+              <MainMenuItem key={item.label} item={item} active={false} handleOnClick={handleOnClick} />
+            ))}
+          </div>
+          <div className={styles.menuGroupLine} />
+        </div>
+      ) : (
+        <div className={styles.mainMenuMobile}>
           {MAINMENU_ITEMS.map(item => (
-            <MainMenuItem key={item.label} item={item} active={activeTab === item.label} handleOnClick={handleOnClick} />
+            <MainMenuItemMobile key={item.label} item={item} active={activeTab === item.label} handleOnClick={handleOnClick} />
           ))}
         </div>
-        <div className={styles.menuGroupLine} />
-        <div className={styles.menuContainer}>
-          {MAINMENU_DESKTOP_ITEMS.map(item => (
-            <MainMenuItem key={item.label} item={item} active={false} handleOnClick={handleOnClick} />
-          ))}
-        </div>
-        <div className={styles.menuGroupLine} />
-      </div>
-      <div className={styles.mainMenuMobile}>
-        {MAINMENU_ITEMS.map(item => (
-          <MainMenuItemMobile key={item.label} item={item} active={activeTab === item.label} handleOnClick={handleOnClick} />
-        ))}
-      </div>
+      )}
     </>
   );
 };
--- src/components/layout/Template.tsx
+++ src/components/layout/Template.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PageHead, MainMenu, NavBar } from '.';
 
 import { PageType } from '../../config/types';
+import { SCREEN_TYPE } from '../../config/constants';
+import useWindowDimensions from '../hooks/useWindowDimensions';
 import styles from './Template.module.css';
 
 interface Props {
@@ -10,20 +12,27 @@ interface Props {
   page?: PageType;
 }
 
-export const Template: React.FC<Props> = ({ title, subTitle, children, page }) => (
-  <div className={styles.container}>
-    <PageHead
-      title={`${title}${subTitle ? `: ${subTitle}` : ''}`} />
-    <div className={styles.mainMenuContainer}>
-      <MainMenu activeTab={page} />
-      <div className={styles.wrapFixed}>
-        <NavBar
-          title={title}
-          subTitle={subTitle} />
+export const Template: React.FC<Props> = ({ title, subTitle, children, page }) => {
+  const [ showDesktop, setShowDesktop ] = useState(false);
+  const { width } = useWindowDimensions();
+  useEffect(() => {
+    setShowDesktop(width > SCREEN_TYPE.PHONE);
+  }, [ width ]);
+  return (
+    <div className={styles.container}>
+      <PageHead
+        title={`${title}${subTitle ? `: ${subTitle}` : ''}`} />
+      <div className={styles.mainMenuContainer}>
+        <MainMenu activeTab={page} showDesktop={showDesktop} />
+        <div className={styles.wrapFixed}>
+          <NavBar
+            title={title}
+            subTitle={subTitle} />
+        </div>
+      </div>
+      <div className={styles.mainContainer}>
+        {children}
       </div>
     </div>
-    <div className={styles.mainContainer}>
-      {children}
-    </div>
-  </div>
-);
+  );
+};
