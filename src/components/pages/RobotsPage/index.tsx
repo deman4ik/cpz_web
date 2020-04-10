c2429f9ae34a76f2899739eaeade0fd6a0609b92
vyuv <kaavolv@gmail.com>
2020-04-10 16:39:10 +0300

    /robots

--- src/components/pages/RobotsPage/index.tsx
+++ src/components/pages/RobotsPage/index.tsx
@@ -1,20 +1,65 @@
 import React from 'react';
-import { Template } from '../../layout/Template';
 
 import useWindowDimensions from '../../../hooks/useWindowDimensions';
+import { useVisibleModal } from '../../../hooks/useVisibleModal';
+import { Template } from '../../layout';
 import { PageType } from '../../../config/types';
+import { PageToolbar } from '../../common';
+import { RobotPerformance } from '../../ui/RobotPerformance';
+//import { ActionRobotModal, EditRobotModal } from '../../ui/Modals';
+import { Modal } from '../../basic';
+import { RobotOpenPositions } from '../../ui/RobotOpenPositions';
+import { SignalRobots } from '../../ui/SignalsRobots';
+import { modalType } from '../types';
+import { getIsVisibleStatus } from '../helpers';
+import styles from './index.module.css';
 
-export const RobotsPage = () => {
+export const RobotsPage: React.FC = () => {
   const { width } = useWindowDimensions();
+  const { titleModal, setTitleModal, dataModal, handleSetVisible } = useVisibleModal();
+
   return (
     <Template
       page={PageType.robots}
       title='Robots'
+      subTitle='Automated Trading'
       width={width}
-  >
-      <div>
-        Robots page
+      toolbar={<PageToolbar displayType='robots' />}
+    >
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <RobotPerformance
+            width={width}
+            type='robots' />
+        </div>
+        <div className={styles.wrapper}>
+          <RobotOpenPositions
+            width={width}
+            type='robots' />
+        </div>
       </div>
+      <SignalRobots width={width} displayType='robots' />
+      {/* <Modal
+        screenType={screenType}
+        onDismiss={handleSetVisible}
+        title={titleModal}
+        visible={getIsVisibleStatus(modalType.action, dataModal)}
+      >
+        <ActionRobotModal
+          onDismiss={handleSetVisible}
+          setTitle={setTitleModal}
+          type={dataModal.ModalVisible.type} />
+      </Modal>
+      <Modal
+        screenType={screenType}
+        visible={getIsVisibleStatus(modalType.edit, dataModal)}
+        title={titleModal}
+        onDismiss={handleSetVisible}
+      >
+        <EditRobotModal
+          setTitle={setTitleModal}
+          onDismiss={handleSetVisible} />
+      </Modal> */}
     </Template>
   );
 };
