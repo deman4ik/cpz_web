5491b56c6b82e250ecc89446d07254efa85d0b80
vyuv <kaavolv@gmail.com>
2020-04-16 13:10:44 +0300

    data exist check

--- src/components/pages/ProfilePage/UserInfo/index.tsx
+++ src/components/pages/ProfilePage/UserInfo/index.tsx
@@ -5,7 +5,7 @@ import dynamic from 'next/dynamic';
 import { GET_USER_INFO } from '../../../../graphql/user/queries';
 import { Button, Modal } from '../../../basic';
 import { InputLike } from '../../../ui/InputLike';
-import { LoadingIndicator } from '../../../common';
+import { LoadingIndicator, NoRecentData } from '../../../common';
 import { NameModal } from './NameModal';
 import { EmailModal } from './EmailModal';
 import { PasswordModal } from './PasswordModal';
@@ -49,86 +49,88 @@ const _UserInfo: React.FC<Props> = ({ width }) => {
         {loading ? (
           <LoadingIndicator />
         ) : (
-          <div className={styles.wrapper}>
-            <div className={styles.container}>
-              <div className={styles.formRow}>
-                <div className={styles.label}>
-                  Username
-                </div>
-                <div className={styles.inputContainer}>
-                  <InputLike
-                    value={data.users[0].name || ''}
-                    onClickButton={handleOnCloseNameModal}
-                    icon='account' />
-                </div>
-              </div>
-              <div className={styles.formRow}>
-                <div className={styles.label}>
-                  Email
-                </div>
-                <div className={styles.inputContainer}>
-                  <InputLike
-                    value={data.users[0].email || ''}
-                    onClickButton={handleOnCloseEmailModal}
-                    icon='email' />
-                </div>
-              </div>
-              {data.users[0].email && (
-              <div className={styles.formRow}>
-                <div className={styles.label}>
-                  Password
-                </div>
-                <div className={styles.inputContainer}>
-                  <Button
-                    title='Change'
-                    type='dimmed'
-                    onClick={handleOnClosePasswordModal}
-                    icon='lockopen' />
-                </div>
-              </div>
-              )}
-              <div className={styles.formRow} style={{ marginBottom: 0 }}>
-                <div className={styles.label}>
-                  Telegram
-                </div>
-                {data.users[0].telegram_id ? (
-                  <div className={styles.inputContainer}>
-                    <div className={styles.telegramName}>
-                      {data.users[0].telegram_username
-                    || data.users[0].telegram_id}
-                    </div>
+          !data ? <NoRecentData message='No data received' /> : (
+            <div className={styles.wrapper}>
+              <div className={styles.container}>
+                <div className={styles.formRow}>
+                  <div className={styles.label}>
+                    Username
                   </div>
-                ) : <TelegramLoginWithNoSSR userId={data.users[0].id} />}
-              </div>
-              <Modal
-                isOpen={isNameModalVisible}
-                title='Change Name'
-                onClose={handleOnCloseNameModal}
-              >
-                <NameModal
-                  name={data.users[0].name || ''}
-                  onClose={handleOnCloseNameModal} />
-              </Modal>
-              <Modal
-                isOpen={isEmailModalVisible}
-                title={title}
-                onClose={handleOnCloseEmailModal}
-              >
-                <EmailModal
-                  width={width}
-                  email={data.users[0].email || ''}
+                  <div className={styles.inputContainer}>
+                    <InputLike
+                      value={data.users[0].name || ''}
+                      onClickButton={handleOnCloseNameModal}
+                      icon='account' />
+                  </div>
+                </div>
+                <div className={styles.formRow}>
+                  <div className={styles.label}>
+                    Email
+                  </div>
+                  <div className={styles.inputContainer}>
+                    <InputLike
+                      value={data.users[0].email || ''}
+                      onClickButton={handleOnCloseEmailModal}
+                      icon='email' />
+                  </div>
+                </div>
+                {data.users[0].email && (
+                <div className={styles.formRow}>
+                  <div className={styles.label}>
+                    Password
+                  </div>
+                  <div className={styles.inputContainer}>
+                    <Button
+                      title='Change'
+                      type='dimmed'
+                      onClick={handleOnClosePasswordModal}
+                      icon='lockopen' />
+                  </div>
+                </div>
+                )}
+                <div className={styles.formRow} style={{ marginBottom: 0 }}>
+                  <div className={styles.label}>
+                    Telegram
+                  </div>
+                  {data.users[0].telegram_id ? (
+                    <div className={styles.inputContainer}>
+                      <div className={styles.telegramName}>
+                        {data.users[0].telegram_username
+                    || data.users[0].telegram_id}
+                      </div>
+                    </div>
+                  ) : <TelegramLoginWithNoSSR userId={data.users[0].id} />}
+                </div>
+                <Modal
+                  isOpen={isNameModalVisible}
+                  title='Change Name'
+                  onClose={handleOnCloseNameModal}
+                >
+                  <NameModal
+                    name={data.users[0].name || ''}
+                    onClose={handleOnCloseNameModal} />
+                </Modal>
+                <Modal
+                  isOpen={isEmailModalVisible}
+                  title={title}
                   onClose={handleOnCloseEmailModal}
-                  setTitle={setTitle} />
-              </Modal>
-              <Modal
-                isOpen={isPasswordModalVisible}
-                title='Change Password'
-                onClose={handleOnClosePasswordModal}
-              >
-                <PasswordModal onClose={handleOnClosePasswordModal} />
-              </Modal>
+                >
+                  <EmailModal
+                    width={width}
+                    email={data.users[0].email || ''}
+                    onClose={handleOnCloseEmailModal}
+                    setTitle={setTitle} />
+                </Modal>
+                <Modal
+                  isOpen={isPasswordModalVisible}
+                  title='Change Password'
+                  onClose={handleOnClosePasswordModal}
+                >
+                  <PasswordModal onClose={handleOnClosePasswordModal} />
+                </Modal>
+              </div>
             </div>
-          </div>
+          )
         )}
       </div>
     </>
