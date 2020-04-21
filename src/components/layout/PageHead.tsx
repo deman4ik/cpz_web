395bff6474d89731cbb9e26bc5fd1ea23aa8b48e
Dmitriy Shatalov <deman4ik@gmail.com>
2020-04-21 15:24:06 +0300

    GA

--- src/components/layout/PageHead.tsx
+++ src/components/layout/PageHead.tsx
@@ -7,20 +7,47 @@ interface Props {
   keywords?: string;
 }
 
-const description = 'Cryptuoso - Cryptocurrency Trading Platform for your successful investment';
-const keywords = 'cryptocurrency, bitcoin, trading, signals, robots, btc, crypto, mining, bitfinex, bitmex, kraken';
+const description =
+  'Cryptuoso - Cryptocurrency Trading Platform for your successful investment';
+const keywords =
+  'cryptocurrency, bitcoin, trading, signals, robots, btc, crypto, mining, bitfinex, bitmex, kraken';
 
 export const PageHead: React.FC<Props> = ({ title }) => (
   <Head>
     <title>CRYPTUOSO - {title}</title>
-    <meta charSet='utf-8' />
-    <meta name='viewport" content="initial-scale=1.0, width=device-width' key='viewport' />
-    <meta name='description' content={description} />
-    <meta name='keywords' content={keywords} />
-    <link rel='shortcut icon' type='image/x-icon' href='/favicon.png' />
+    <meta charSet="utf-8" />
+    <meta
+      name='viewport" content="initial-scale=1.0, width=device-width'
+      key="viewport"
+    />
+    <meta name="description" content={description} />
+    <meta name="keywords" content={keywords} />
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.png" />
     <link
-      href='https://fonts.googleapis.com/css?family=Roboto:400,700'
-      rel='stylesheet'
+      href="https://fonts.googleapis.com/css?family=Roboto:400,700"
+      rel="stylesheet"
+    />
+    {/* Google Page Speed */}
+    <link
+      rel="preload"
+      href="/_next/static/chunks/fonts/MaterialCommunityIcons-5a293a273bee8d740a045d9922b9a9ae.ttf"
+      as="font"
+      type="font/ttf"
+      crossOrigin="anonymous"
+    />
+    {/*  Global site tag (gtag.js) - Google Analytics */}
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-37BGBQ6GCK"
+    />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-37BGBQ6GCK');`,
+      }}
     />
   </Head>
 );
