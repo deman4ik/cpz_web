3c1bfa41c3a7b9706b7efd6325175121e4c9069e
Dmitriy Shatalov <deman4ik@gmail.com>
2020-04-29 18:22:36 +0300

    open positions total

--- src/components/ui/RobotOpenPositions/helpers.ts
+++ src/components/ui/RobotOpenPositions/helpers.ts
@@ -1,12 +1,22 @@
 import { color } from '../../../config/constants';
-import { formatDate, moneyFormat } from '../../../config/utils';
+import { formatDate, moneyFormat, round } from '../../../config/utils';
 
-export const getColor = (condition: boolean) => (condition ? color.negative : color.positive);
-export const getIconName = (direction: string) => (direction === 'short' ? 'arrow-down' : 'arrow-up');
+export const getColor = (condition: boolean) =>
+  condition ? color.negative : color.positive;
+export const getIconName = (direction: string) =>
+  direction === 'short' ? 'arrow-down' : 'arrow-up';
 
 const getRobotDataSignals = (position) => {
-  const { id, code, direction, entry_date, entry_price, robot, user_signal } = position;
-  return ({
+  const {
+    id,
+    code,
+    direction,
+    entry_date,
+    entry_price,
+    robot,
+    user_signal,
+  } = position;
+  return {
     id,
     code,
     volume: user_signal.volume,
@@ -16,40 +26,57 @@ const getRobotDataSignals = (position) => {
     robot: {
       name: robot.name,
       code: robot.code,
-      asset: robot.asset
-    }
-  });
+      asset: robot.asset,
+    },
+  };
 };
 
-export const getFormatDataSignals = (positions: any) => positions.reduce((acc, position) => {
-  const item = acc.find(el => el.exchange === position.robot.exchange);
-  const obj = {
-    exchange: position.robot.exchange,
-    assets: []
-  };
-  const robot = getRobotDataSignals(position);
-  const asset = {
-    asset: position.robot.asset,
-    volume: (position.direction === 'short' ? -1 : 1) * position.user_signal.volume,
-    robots: [ robot ]
-  };
-  if (item) {
-    const findAsset = item.assets.find(el => el.asset === position.robot.asset);
-    if (!findAsset) {
-      item.assets.push(asset);
+export const getFormatDataSignals = (positions: any) =>
+  positions.reduce((acc, position) => {
+    const item = acc.find((el) => el.exchange === position.robot.exchange);
+    const obj = {
+      exchange: position.robot.exchange,
+      assets: [],
+    };
+    const robot = getRobotDataSignals(position);
+    const asset = {
+      asset: position.robot.asset,
+      volume:
+        (position.direction === 'short' ? -1 : 1) * position.user_signal.volume,
+      robots: [robot],
+    };
+    if (item) {
+      const findAsset = item.assets.find(
+        (el) => el.asset === position.robot.asset
+      );
+      if (!findAsset) {
+        item.assets.push(asset);
+      } else {
+        findAsset.robots.push(robot);
+        findAsset.volume = round(
+          findAsset.volume +
+            (robot.direction === 'short' ? -1 : 1) * robot.volume,
+          6
+        );
+      }
     } else {
-      findAsset.robots.push(robot);
-      findAsset.volume = Math.floor((findAsset.volume + (robot.direction === 'short' ? -1 : 1) * robot.volume) * 1000) / 1000;
+      obj.assets = [asset];
     }
-  } else {
-    obj.assets = [ asset ];
-  }
-  return (item ? acc : [ ...acc, obj ]);
-}, []);
+    return item ? acc : [...acc, obj];
+  }, []);
 
 const getRobotDataRobots = (position) => {
-  const { id, code, direction, entry_date, entry_price, volume, asset, user_robot } = position;
-  return ({
+  const {
+    id,
+    code,
+    direction,
+    entry_date,
+    entry_price,
+    volume,
+    asset,
+    user_robot,
+  } = position;
+  return {
     id,
     code,
     volume,
@@ -59,38 +86,43 @@ const getRobotDataRobots = (position) => {
     robot: {
       name: user_robot.robot.name,
       code: user_robot.robot.code,
-      asset
-    }
-  });
+      asset,
+    },
+  };
 };
 
-export const getFormatDataRobots = (positions: any) => positions.reduce((acc, position) => {
-  const item = acc.find(el => el.exchange === position.exchange);
-  const obj = {
-    exchange: position.exchange,
-    assets: []
-  };
-  const robot = getRobotDataRobots(position);
-  const asset = {
-    asset: position.asset,
-    volume: (position.direction === 'short' ? -1 : 1) * position.volume,
-    robots: [ robot ]
-  };
-  if (item) {
-    const findAsset = item.assets.find(el => el.asset === position.asset);
-    if (!findAsset) {
-      item.assets.push(asset);
+export const getFormatDataRobots = (positions: any) =>
+  positions.reduce((acc, position) => {
+    const item = acc.find((el) => el.exchange === position.exchange);
+    const obj = {
+      exchange: position.exchange,
+      assets: [],
+    };
+    const robot = getRobotDataRobots(position);
+    const asset = {
+      asset: position.asset,
+      volume: (position.direction === 'short' ? -1 : 1) * position.volume,
+      robots: [robot],
+    };
+    if (item) {
+      const findAsset = item.assets.find((el) => el.asset === position.asset);
+      if (!findAsset) {
+        item.assets.push(asset);
+      } else {
+        findAsset.robots.push(robot);
+        findAsset.volume = round(
+          findAsset.volume +
+            (robot.direction === 'short' ? -1 : 1) * robot.volume,
+          6
+        );
+      }
     } else {
-      findAsset.robots.push(robot);
-      findAsset.volume = Math.floor((findAsset.volume + (robot.direction === 'short' ? -1 : 1) * robot.volume) * 1000) / 1000;
+      obj.assets = [asset];
     }
-  } else {
-    obj.assets = [ asset ];
-  }
-  return (item ? acc : [ ...acc, obj ]);
-}, []);
+    return item ? acc : [...acc, obj];
+  }, []);
 
 export const title = {
   signals: 'My Signals Open Positions',
-  robots: 'My Robots Open Positions'
+  robots: 'My Robots Open Positions',
 };
--- src/config/utils.ts
+++ src/config/utils.ts
@@ -1,19 +1,29 @@
 import dayjs from '../libs/dayjs';
 import { timeFrameFormat, color } from './constants';
 
-export const moneyFormat = (value: number, toFixed = 2): string => (
-  !value ? '0' : value.toFixed(toFixed).toString()
-    .replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+export const moneyFormat = (value: number, toFixed = 2): string =>
+  !value
+    ? '0'
+    : value
+        .toFixed(toFixed)
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+export const round = (n: number, decimals = 0): number =>
+  +Number(`${Math.round(+`${n}e${decimals}`)}e-${decimals}`);
 
 export const valueWithSign = (value: number | string): string =>
-  (Number(value) > 0 ? `+${value}` : `${value}`);
+  Number(value) > 0 ? `+${value}` : `${value}`;
 
 export const capitalize = (s: string) => {
   if (typeof s !== 'string') return '';
   return s.charAt(0).toUpperCase() + s.slice(1);
 };
 
-export const splitCapitaize = (s: string): string => capitalize(s).split(/(?=[A-Z])/).join(' ');
+export const splitCapitaize = (s: string): string =>
+  capitalize(s)
+    .split(/(?=[A-Z])/)
+    .join(' ');
 
 export const formatDate = (date: string, addUTC = true): string => {
   const res = dayjs.utc(date).format('DD MMM YY HH:mm');
@@ -21,22 +31,37 @@ export const formatDate = (date: string, addUTC = true): string => {
   return addUTC ? `${res} UTC` : res;
 };
 
-export const exchangeName = (code: string) => (
-  code ? code.split('_').map(word => capitalize(word)).join(' ') : code
-);
+export const exchangeName = (code: string) =>
+  code
+    ? code
+        .split('_')
+        .map((word) => capitalize(word))
+        .join(' ')
+    : code;
 
 export const getLegend = (robot) =>
-  `${exchangeName(robot.exchange)} ${robot.asset}/${robot.currency} ${timeFrameFormat[robot.timeframe].abbr}`;
+  `${exchangeName(robot.exchange)} ${robot.asset}/${robot.currency} ${
+    timeFrameFormat[robot.timeframe].abbr
+  }`;
 
-export const roundFormat = (value: number): number => Math.round(value * 100) / 100;
+export const roundFormat = (value: number): number =>
+  Math.round(value * 100) / 100;
 
-export const colorAction = (check: boolean): object => ({ color: check ? color.positive : color.negative });
-export const colorDirection = (direction: string): object =>
-  ({ color: [ 'long', 'closeShort' ].includes(direction) ? color.positive : color.negative });
+export const colorAction = (check: boolean): object => ({
+  color: check ? color.positive : color.negative,
+});
+export const colorDirection = (direction: string): object => ({
+  color: ['long', 'closeShort'].includes(direction)
+    ? color.positive
+    : color.negative,
+});
 
-export const getColor = (condition: boolean) => (condition ? color.negative : color.positive);
-export const getIconName = (direction: string) => (direction === 'short' ? 'arrowdown' : 'arrowup');
-export const getIconNameAction = (check: boolean) => (!check ? 'arrowdown' : 'arrowup');
+export const getColor = (condition: boolean) =>
+  condition ? color.negative : color.positive;
+export const getIconName = (direction: string) =>
+  direction === 'short' ? 'arrowdown' : 'arrowup';
+export const getIconNameAction = (check: boolean) =>
+  !check ? 'arrowdown' : 'arrowup';
 
 export const truncate = (str: string, maxLength: number) => {
   if (str.length <= maxLength) return str;
@@ -50,13 +75,16 @@ export const truncate = (str: string, maxLength: number) => {
 export const getHash = (length) => {
   const radom13chars = () => Math.random().toString(16).substring(2, 15);
   const loops = Math.ceil(length / 13);
-  return new Array(loops).fill(radom13chars).reduce((string, func) => string + func(), '').substring(0, length);
+  return new Array(loops)
+    .fill(radom13chars)
+    .reduce((string, func) => string + func(), '')
+    .substring(0, length);
 };
 
 export const getSearchProps = (data, displayType) => {
   let result = null;
   if (data && data.SearchProps.props) {
-    result = data.SearchProps.props.find(el => el.type === displayType);
+    result = data.SearchProps.props.find((el) => el.type === displayType);
   }
   return result;
 };
