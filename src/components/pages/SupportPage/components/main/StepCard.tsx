abf9d01f1bd975a1756a9264a650d315116f8aa2
Alexandr Kudryashov <kudryashov_as@astral.ru>
2020-06-10 20:36:39 +0300

    #114 redirect to pages

--- src/components/pages/SupportPage/components/main/StepCard.tsx
+++ src/components/pages/SupportPage/components/main/StepCard.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import Link from "next/link";
 // styles
 import styles from "../../styles/Main.module.css";
 // contants & types
 import { stepsCard, STEP_CARD_ICONS } from "./constants";
 
 const StepCard: React.FC<stepsCard> = ({ title, link, icon }) => {
+    const handleClick = () => {
+        window.location.href = link;
+    };
+
     const alignContent: React.CSSProperties = { justifyContent: icon ? "baseline" : "center" };
+
     return (
-        <Link href={link}>
-            <div className={styles.step_card} style={alignContent}>
-                {icon && <div className={styles.step_card_icon}>{STEP_CARD_ICONS[icon]}</div>}
-                <div>{title}</div>
-            </div>
-        </Link>
+        <div className={styles.step_card} style={alignContent} onClick={handleClick}>
+            {icon && <div className={styles.step_card_icon}>{STEP_CARD_ICONS[icon]}</div>}
+            <div>{title}</div>
+        </div>
     );
 };
 
--- src/components/pages/SupportPage/components/main/index.tsx
+++ src/components/pages/SupportPage/components/main/index.tsx
@@ -6,10 +6,17 @@ import styles from "../../styles/Common.module.css";
 import stylesMain from "../../styles/Main.module.css";
 // contants
 import { STEPS_CARDS } from "./constants";
+import { TELEGRAM_COMMUNITY_URL } from "config/constants";
 
+/*Карточки ссылками на документацию*/
 const StepsCards = STEPS_CARDS.map((item, index) => <StepCard {...item} key={index} />);
 
 const Main: React.FC = () => {
+    /*TG community link open*/
+    const handleTGClick = () => {
+        window.location.href = TELEGRAM_COMMUNITY_URL;
+    };
+
     return (
         <div className={styles.content_container}>
             <div className={styles.section_title}>First steps</div>
@@ -17,7 +24,7 @@ const Main: React.FC = () => {
             <div className={styles.section_title}>Contact Support</div>
             <div className={stylesMain.contact_support_card}>
                 <div className={stylesMain.contact_support_card_title}>Telegram Community Support</div>
-                <div className={stylesMain.contact_support_card_description}>
+                <div className={stylesMain.contact_support_card_description} onClick={handleTGClick}>
                     Having common questions with signals or robots? <br />
                     Ask it in our Telegram Community and we will help you.
                 </div>
