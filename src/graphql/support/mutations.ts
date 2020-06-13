f3256de9017a853b348a32dc025b8ac5b3f433a2
Alexandr Kudryashov <kudryashov_as@astral.ru>
2020-06-13 21:50:49 +0300

    fix with lint

--- src/graphql/support/mutations.ts
+++ src/graphql/support/mutations.ts
@@ -2,7 +2,7 @@ import gql from "graphql-tag";
 
 export const SEND_SUPPOT_MESSAGE = gql`
     mutation sendSupportMessage($message: String!) {
-      supportMessage(message: $message) {
+        supportMessage(message: $message) {
             success
             result
             error
