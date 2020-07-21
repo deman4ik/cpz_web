10f06de642b8a163f0543c36611b31df13916191
modecry <MynuberIs7676>
2020-07-21 16:46:20 +0300

    comment manage queries

--- src/graphql/manage/queries.ts
+++ src/graphql/manage/queries.ts
@@ -1,5 +1,9 @@
 import gql from "graphql-tag";
 
+/**
+ * Запрос на получение  количества user_robots user_signals
+ * Использование:  manage/dashboard
+ */
 export const GET_USER_STATS = gql`
     query {
         users {
@@ -13,6 +17,11 @@ export const GET_USER_STATS = gql`
     }
 `;
 
+/**
+ *  Получение данных за определенный период
+ *  @period - дата за определенный период
+ *  Использование:  manage/dashboard
+ */
 export const GET_USER_STATS_DURING_PERIOD = gql`
     query getUsersStatsDuringPeriod($period: timestamp) {
         users(where: { created_at: { _gte: $period } }) {
@@ -21,6 +30,13 @@ export const GET_USER_STATS_DURING_PERIOD = gql`
     }
 `;
 
+/**
+ * Получение данных юзерам
+ * @where -  фильтрация
+ * @order_by - сортировка
+ * @limit - для пагинации
+ * Использование:  manage/users
+ */
 export const GET_USERS = gql`
     query getUsers($where: users_bool_exp, $order_by: [users_order_by!], $limit: Int) {
         users(limit: $limit, where: $where, order_by: $order_by) {
@@ -61,6 +77,11 @@ export const GET_USERS = gql`
     }
 `;
 
+
+/**
+ *  Получение значения по общему количеству users
+ *  Использование:  manage/users
+ */
 export const USERS_AGGREGATE = gql`
     query users_aggr {
         users_aggregate {
@@ -71,6 +92,13 @@ export const USERS_AGGREGATE = gql`
     }
 `;
 
+/**
+ * Получение данных по всем  robots
+ * @where -  фильтрация
+ * @order_by - сортировка
+ * @limit - для пагинации
+ * Использование:  manage/robots
+ */
 export const GET_ROBOTS = gql`
     query get_robots($limit: Int, $where: robots_bool_exp, $order_by: [robots_order_by!]) {
         robots(limit: $limit, where: $where, order_by: $order_by) {
@@ -98,6 +126,10 @@ export const GET_ROBOTS = gql`
     }
 `;
 
+/**
+ *  Получение значения по общему количеству robots
+ *  Использование:  manage/users
+ */
 export const ROBOTS_AGGREGATE = gql`
     query robots_aggr {
         robots_aggregate {
@@ -108,6 +140,13 @@ export const ROBOTS_AGGREGATE = gql`
     }
 `;
 
+/**
+ * Получение данных  user_signals
+ * @where -  фильтрация
+ * @order_by - сортировка
+ * @limit - для пагинации
+ * Использование: manage/user_signals
+ */
 export const GET_USER_SIGNALS = gql`
     query get_user_signals($limit: Int, $where: user_signals_bool_exp, $order_by: [user_signals_order_by!]) {
         user_signals(limit: $limit, where: $where, order_by: $order_by) {
@@ -125,6 +164,10 @@ export const GET_USER_SIGNALS = gql`
     }
 `;
 
+/**
+ *  Получение значения по общему количеству user_signals
+ *  Использование:  manage/user_signals
+ */
 export const USER_SIGNALS_AGGREGATE = gql`
     query user_signals_aggr {
         user_signals_aggregate {
@@ -135,6 +178,13 @@ export const USER_SIGNALS_AGGREGATE = gql`
     }
 `;
 
+/**
+ * Получение данных  user_robots
+ * @where -  фильтрация
+ * @order_by - сортировка
+ * @limit - для пагинации
+ * Использование:  manage/user_robots
+ */
 export const GET_USER_ROBOTS = gql`
     query get_user_robots($limit: Int, $where: user_robots_bool_exp, $order_by: [user_robots_order_by!]) {
         user_robots(limit: $limit, where: $where, order_by: $order_by) {
@@ -159,6 +209,11 @@ export const GET_USER_ROBOTS = gql`
     }
 `;
 
+
+/**
+ *  Получение значения по общему количеству user_robots
+ *  Использование:  manage/user_signals
+ */
 export const USER_ROBOTS_AGGREGATE = gql`
     query user_robots_aggr {
         user_robots_aggregate {
@@ -169,6 +224,10 @@ export const USER_ROBOTS_AGGREGATE = gql`
     }
 `;
 
+/**
+ *  Запрос на получение данных по фильтрам
+ *  Использование:  manage/robots
+ */
 export const GET_ROBOTS_STATS = gql`
     query get_manage_robots_filters {
         stats: robots(distinct_on: [exchange, asset, currency, strategy, timeframe, trading, status]) {
@@ -183,6 +242,11 @@ export const GET_ROBOTS_STATS = gql`
     }
 `;
 
+
+/**
+ *  Запрос на получение данных по фильтрам
+ *  Использование:  manage/user_robots
+ */
 export const GET_USER_ROBOTS_STATS = gql`
     query get_user_robots_filters {
         stats: robots(distinct_on: [exchange, asset]) {
