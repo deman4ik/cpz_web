10af3c486836862ed00d4b18e642b70d88b3d2d0
modecry <MynuberIs7676>
2020-07-29 16:23:45 +0300

    revert other

--- src/components/pages/ManagePage/index.tsx
+++ src/components/pages/ManagePage/index.tsx
@@ -4,6 +4,5 @@ import ManageUsers from "./users";
 import ManageRobots from "./robots";
 import ManageUserSignals from "./userSignals";
 import ManageUserRobots from "./userRobots";
-import ManageSupportRequests from "./supportRequests";
 
-export { MangeDashboard, ManageUsers, ManageRobots, ManageUserSignals, ManageUserRobots, ManageSupportRequests };
+export { MangeDashboard, ManageUsers, ManageRobots, ManageUserSignals, ManageUserRobots };
