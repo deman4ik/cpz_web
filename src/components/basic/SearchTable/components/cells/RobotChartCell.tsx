df206e79c9e39e4a223392d02b5e51fc3f256c59
Dmitriy Shatalov <deman4ik@gmail.com>
2020-09-02 02:08:57 +0300

    #102 (#181)
    * different colors for positive and negative values in performance chart
    
    * use gradient
    
    * fix build
    
    * fix build
    
    * fix height of AreaChart
    
    Co-authored-by: Arutyun Mnatsakanyan <arutyun.mnatsakanyan@gmail.com>

--- src/components/basic/SearchTable/components/cells/RobotChartCell.tsx
+++ src/components/basic/SearchTable/components/cells/RobotChartCell.tsx
@@ -15,7 +15,7 @@ interface RobotChartCellProps {
 const RobotChartCell: React.FC<RobotChartCellProps> = ({ perfomance, height, profit, style }) => {
     return (
         <div style={style} className={styles.robot_chart_cell}>
-            <AreaChart height={height} positive={profit > 0} data={perfomance} />
+            <AreaChart height={height} data={perfomance} />
         </div>
     );
 };
