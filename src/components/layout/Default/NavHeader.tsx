7fd5e09663c62c11dc9d0c7e63e8aacddd9ec232
VanillinT <tominec98@gmail.com>
2020-09-17 20:51:20 +0300

    Add responsive title visibility

--- src/components/layout/Default/NavHeader.tsx
+++ src/components/layout/Default/NavHeader.tsx
@@ -1,27 +1,41 @@
-import React, { memo } from "react";
+import React, { memo, useRef } from "react";
 import { EffectButton } from "components/basic/EffectButton";
 
 import styles from "./styles/NavHeader.module.css";
+import useWindowDimensions from "hooks/useWindowDimensions";
 
 interface Props {
     title: string;
     subTitle?: string;
-    toolbar: any;
+    toolbar?: any;
     handlePressBack: () => void;
     style?: React.CSSProperties;
 }
 
-const _NavHeader: React.FC<Props> = ({ title, subTitle, toolbar, handlePressBack, style }) => (
-    <div className={styles.header} style={style}>
-        <div className={styles.wrapper}>
-            {handlePressBack ? <EffectButton onClick={handlePressBack} icon="arrowleft" /> : null}
-            <div className={styles.titleGroup} style={{ marginLeft: handlePressBack ? 38 : 10 }}>
-                <div className={styles.title}>{title}</div>
-                <div className={styles.subTitle}>{subTitle}</div>
+const _NavHeader: React.FC<Props> = ({ title, subTitle, toolbar, handlePressBack, style }) => {
+    const toolbarContainer = useRef(null);
+    const toolbarWidth = toolbarContainer.current?.getBoundingClientRect().width;
+    const { width: clientWidth } = useWindowDimensions();
+
+    return (
+        <div className={styles.header} style={style}>
+            <div className={styles.wrapper}>
+                {handlePressBack && <EffectButton onClick={handlePressBack} icon="arrowleft" />}
+                <div
+                    className={styles.titleGroup}
+                    style={{
+                        marginLeft: handlePressBack ? 38 : 10,
+                        display: clientWidth < toolbarWidth * 1.5 ? "none" : "flex"
+                    }}>
+                    <div className={styles.title}>{title}</div>
+                    <div className={styles.subTitle}>{subTitle}</div>
+                </div>
+                <div ref={toolbarContainer} className={styles.toolbarContainer}>
+                    {toolbar}
+                </div>
             </div>
-            {toolbar}
         </div>
-    </div>
-);
+    );
+};
 
 export const NavHeader = memo(_NavHeader);
