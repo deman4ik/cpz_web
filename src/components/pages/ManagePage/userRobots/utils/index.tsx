a79885a86276f3619a4761c8966191f8ec53addc
VanillinT <tominec98@gmail.com>
2020-10-16 23:29:39 +0300

    improve code structure

--- src/components/pages/ManagePage/userRobots/utils/index.tsx
+++ src/components/pages/ManagePage/userRobots/utils/index.tsx
@@ -1,25 +1,25 @@
 import { formatDate, getStats } from "config/utils";
 
 export const formatUserRobots = ({ user_robots }: { user_robots: any }): any => {
-    return user_robots.map((robot) => {
-        console.log(robot);
-        const { equity, profit, winRate, maxDrawdown, tradesCount } = getStats(robot);
+    return user_robots.map((user_robot) => {
+        const { created_at, stopped_at, status, user_robot_settings, user, robot } = user_robot;
+        const { equity, profit, winRate, maxDrawdown, tradesCount } = getStats(user_robot);
 
         return {
-            created_at: formatDate(robot?.created_at),
-            stopped_at: formatDate(robot?.stopped_at),
+            created_at: formatDate(created_at),
+            stopped_at: formatDate(stopped_at),
 
             performance: equity,
             maxDrawdown,
             profit,
             tradesCount,
             winRate,
-            robot_code: robot?.robot?.name,
-            robot_id: robot?.robot?.id,
-            volume: robot?.user_robot_settings?.user_robot_settings.volume,
-            user_name: robot?.user?.name,
-            user_id: robot?.user?.id,
-            status: robot?.status
+            volume: user_robot_settings?.user_robot_settings.volume,
+            robot_code: robot?.name,
+            robot_id: robot?.id,
+            user_name: user?.name,
+            user_id: user?.id,
+            status
         };
     });
 };
