4ff0641022f613c0789f040fd57e587682f9e9ee
VanillinT <tominec98@gmail.com>
2020-10-19 18:13:44 +0300

    finalize

--- src/components/pages/TradingRobotsSearchPage/helpers.ts
+++ src/components/pages/TradingRobotsSearchPage/helpers.ts
@@ -2,59 +2,61 @@ import { getStats, getVolumeWithUnit } from "config/utils";
 import dayjs from "libs/dayjs";
 
 // TODO: refactor
-export const formatRobotsData = (stats: any) =>
-    stats.map((el: any) => {
-        const { id, code, name, exchange, asset, currency, active, user_robots } = el.robot;
-        const {
-            robot_settings: { robot_settings }
-        } = el.robot;
-        const userRobot = user_robots && user_robots[0];
-        const { equity, profit, winRate, maxDrawdown, tradesCount } = getStats(el.robot);
-        const res = {
-            cache: {
-                id,
-                tableName: "robots"
-            },
-            id,
-            code,
-            name,
-            exchange,
-            asset,
-            currency,
-            user_robots: {
-                status: null,
-                id: null
-            },
-            started_at: null,
-            volume: getVolumeWithUnit(robot_settings, { currency, asset }),
-            profit,
-            performance: equity,
-            active: active ? dayjs.utc(active).fromNow(true) : active,
-            isSubscribed: false,
-            winRate,
-            maxDrawdown,
-            tradesCount
-        };
-
-        if (userRobot) {
+export const formatRobotsData = (data: any) =>
+    data
+        .filter((el) => el.robot)
+        .map(({ robot }: any) => {
+            const { id, code, name, exchange, asset, currency, active, user_robots } = robot;
             const {
-                equity: signalEquity,
-                profit: signalProfit,
-                winRate: signalWinRate,
-                maxDrawdown: signalMaxDrawdown,
-                tradesCount: signalTradesCount
-            } = getStats(userRobot);
+                robot_settings: { robot_settings }
+            } = robot;
+            const userRobot = user_robots && user_robots[0];
+            const { equity, profit, winRate, maxDrawdown, tradesCount } = getStats(robot);
+            const res = {
+                cache: {
+                    id,
+                    tableName: "robots"
+                },
+                id,
+                code,
+                name,
+                exchange,
+                asset,
+                currency,
+                user_robots: {
+                    status: null,
+                    id: null
+                },
+                started_at: null,
+                volume: getVolumeWithUnit(robot_settings, { currency, asset }),
+                profit,
+                performance: equity,
+                active: active ? dayjs.utc(active).fromNow(true) : active,
+                isSubscribed: false,
+                winRate,
+                maxDrawdown,
+                tradesCount
+            };
 
-            res.user_robots.status = userRobot.status;
-            res.user_robots.id = userRobot.id;
-            res.volume = `${userRobot.user_robot_settings.user_robot_settings.volume} ${asset}`;
-            res.started_at = userRobot.started_at ? dayjs.utc(userRobot.started_at).fromNow(true) : 0;
-            res.performance = signalEquity;
-            res.winRate = signalWinRate;
-            res.maxDrawdown = signalMaxDrawdown;
-            res.tradesCount = signalTradesCount;
-            res.profit = signalProfit;
-        }
+            if (userRobot) {
+                const {
+                    equity: signalEquity,
+                    profit: signalProfit,
+                    winRate: signalWinRate,
+                    maxDrawdown: signalMaxDrawdown,
+                    tradesCount: signalTradesCount
+                } = getStats(userRobot);
 
-        return res;
-    });
+                res.user_robots.status = userRobot.status;
+                res.user_robots.id = userRobot.id;
+                res.volume = `${userRobot.user_robot_settings.user_robot_settings.volume} ${asset}`;
+                res.started_at = userRobot.started_at ? dayjs.utc(userRobot.started_at).fromNow(true) : 0;
+                res.performance = signalEquity;
+                res.winRate = signalWinRate;
+                res.maxDrawdown = signalMaxDrawdown;
+                res.tradesCount = signalTradesCount;
+                res.profit = signalProfit;
+            }
+
+            return res;
+        });
