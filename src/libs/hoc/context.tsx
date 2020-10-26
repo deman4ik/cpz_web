7168e233c09a75155451784abafa4ca8ad860a78
Andrey <andreysakirkin@gmail.com>
2020-10-26 13:15:51 +0300

    quick fix

--- src/libs/hoc/context.tsx
+++ src/libs/hoc/context.tsx
@@ -1,13 +1,13 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useCallback } from "react";
 
 export const AuthContext = createContext({ authState: null, setAuthState: null });
 
 /*Компонент провадйер контекста*/
 export const AuthContextProvider: React.FC = (props: any) => {
     const [authState, setState] = useState({ isAuth: false });
-    function setAuthState(params: any) {
+    const setAuthState = useCallback((params: any) => {
         setState({ ...params, authIsSet: true });
-    }
+    }, []);
     return <AuthContext.Provider value={{ authState, setAuthState }}>{props.children}</AuthContext.Provider>;
 };
 
