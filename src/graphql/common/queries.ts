c553a1adf2d0e76ac7197f76adecd9f1ff74fdd3
Dmitriy <deman4ik@gmail.com>
2020-11-20 16:15:33 +0300

    fix: get_user_markets query | DEPLOY |

--- src/graphql/common/queries.ts
+++ src/graphql/common/queries.ts
@@ -4,14 +4,14 @@ export const GET_MARKETS = gql`
     query get_user_markets($user_id: uuid!, $asset: String!, $exchange: String!, $currency: String!) {
         v_user_exchange_accs(where: { status: { _eq: "enabled" }, user: { id: { _eq: $user_id } } }) {
             total_balance_usd
+            amounts {
+                    used_balance_percent
+                }
             user {
                 markets(where: { asset: { _eq: $asset }, exchange: { _eq: $exchange }, currency: { _eq: $currency } }) {
                     asset
                     current_price
                     limits
-                }
-                amounts {
-                    used_balance_percent
                 }
             }
         }
