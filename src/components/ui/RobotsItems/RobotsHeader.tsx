111168a6e78f8c70b98278e796f5c48ce12c0369
VanillinT <tominec98@gmail.com>
2020-12-08 11:16:28 +0300

    widen profit in robots list (#464)

--- src/components/ui/RobotsItems/RobotsHeader.tsx
+++ src/components/ui/RobotsItems/RobotsHeader.tsx
@@ -3,22 +3,22 @@ import styles from "./RobotsHeader.module.css";
 
 const _RobotsHeader: React.FC = () => (
     <div className={styles.container}>
-        <div className={styles.title} style={{ minWidth: 220, flex: 25 }}>
+        <div className={styles.title} style={{ flex: 4 }}>
             Robot Name
         </div>
-        <div className={styles.title} style={{ flex: 25 }}>
+        <div className={styles.title} style={{ flex: 4.2 }}>
             Performance
         </div>
-        <div className={styles.title} style={{ flex: 8 }}>
+        <div className={styles.title} style={{ flex: 2 }}>
             Profit
         </div>
-        <div className={styles.title} style={{ flex: 17 }}>
+        <div className={styles.title} style={{ flex: 2.8 }}>
             Statistics
         </div>
-        <div className={styles.title} style={{ flex: 13 }}>
+        <div className={styles.title} style={{ flex: 2.2 }}>
             Status
         </div>
-        <div className={styles.title} style={{ flex: 12 }}>
+        <div className={styles.title} style={{ flex: 2 }}>
             Action
         </div>
     </div>
