bb9f320ce09cb0fc544b360398e0dac2ddb2d05c
Ilya Krupin <dev@krupibox.ru>
2021-01-21 15:47:24 +0300

    Add env variable for anaytics. Add Yandex and Facebook counters

--- src/components/layout/PageHead.tsx
+++ src/components/layout/PageHead.tsx
@@ -26,19 +26,70 @@ export const PageHead: React.FC<Props> = ({ title, gtag }) => (
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
         {/*  Global site tag (gtag.js) - Google Analytics */}
-        {process.env.NODE_ENV === "development" ? null : (
+        {process.env.ENABLE_ANALYTICS === "development" ? null : (
             <>
                 <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
                 <script
                     dangerouslySetInnerHTML={{
                         __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_TRACKING_ID}', {
-            page_path: window.location.pathname,
-          });
-          gtag('config', '${AW_CONVERSION_ID}');${gtag || ""}`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${GA_TRACKING_ID}', {
+                        page_path: window.location.pathname,
+                        });
+                        gtag('config', '${AW_CONVERSION_ID}');${gtag || ""}`
+                    }}
+                />
+            </>
+        )}
+        {process.env.ENABLE_ANALYTICS === "development" ? null : (
+            <>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                        ym(69520861, "init", {
+                        clickmap:true,
+                        trackLinks:true,
+                            accurateTrackBounce:true,
+                        webvisor:true
+                        });`
+                    }}
+                />
+                <noscript
+                    dangerouslySetInnerHTML={{
+                        __html: `<div><img src="https://mc.yandex.ru/watch/69520861" style="position:absolute; left:-9999px;" alt="" /></div>`
+                    }}
+                />
+            </>
+        )}
+        {process.env.ENABLE_ANALYTICS === "development" ? null : (
+            <>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        !function(f,b,e,v,n,t,s)
+                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                        n.queue=[];t=b.createElement(e);t.async=!0;
+                        t.src=v;s=b.getElementsByTagName(e)[0];
+                        s.parentNode.insertBefore(t,s)}(window, document,'script',
+                        'https://connect.facebook.net/en_US/fbevents.js');
+                        fbq('init', '3892002647514527');
+                        fbq('track', 'PageView');`
+                    }}
+                />
+                <noscript
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        <img height="1" width="1" style="display:none"
+                        src="https://www.facebook.com/tr?id=3892002647514527&ev=PageView&noscript=1"
+                        />`
                     }}
                 />
             </>
