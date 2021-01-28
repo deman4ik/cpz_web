c14c66a97af3af8d97684d7434dc54108201f0d4
Ilya Krupin <dev@krupibox.ru>
2021-01-28 12:01:01 +0300

    Delete margin-top

--- src/components/common/Delimiter.tsx
+++ src/components/common/Delimiter.tsx
@@ -1,10 +1,6 @@
 import styles from "components/ui/Modals/index.module.css";
 import React, { FC } from "react";
 
-export const Delimiter: FC<{ first?: boolean }> = ({ first }) => {
-    return (
-        <span className={styles.delimiter} style={{ marginTop: first ? 0 : 10 }}>
-            ≈
-        </span>
-    );
+export const Delimiter: FC<{ first?: boolean }> = () => {
+    return <span className={styles.delimiter}>≈</span>;
 };
