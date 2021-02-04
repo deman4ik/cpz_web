dca3f7261e7f8cd9a54a322376b26909d53c1ae9
Ilya Krupin <dev@krupibox.ru>
2021-02-04 16:04:14 +0300

    Update slider for our happy friends

--- src/components/pages/LandingPage/OurHappyUsers/index.tsx
+++ src/components/pages/LandingPage/OurHappyUsers/index.tsx
@@ -1,16 +1,29 @@
 import React, { memo } from "react";
 import Slider from "react-slick";
 import styles from "./index.module.css";
-import { FoundersItem } from "../Founders/FoundersItem";
+import { OurHappyUser } from "./OurHappyUser";
 import { foundersContent } from "../Founders/helpres";
+
+import { ArrowSliderIcon } from "assets/icons/svg";
+
+function SliderArrow(props) {
+    const { className, onClick } = props;
+    return (
+        <div className={className} onClick={onClick}>
+            <ArrowSliderIcon rotate={props.rotate} />
+        </div>
+    );
+}
 
 const _OurHappyUsers = () => {
     const settings = {
-        dots: false,
+        dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 2,
         slidesToScroll: 1,
+        nextArrow: <SliderArrow rotate="rotate(180deg)" />,
+        prevArrow: <SliderArrow />,
         responsive: [
             {
                 breakpoint: 1024,
@@ -30,10 +43,10 @@ const _OurHappyUsers = () => {
             <div className={styles.aboutus}>
                 <Slider {...settings}>
                     {foundersContent.map((item) => (
-                        <FoundersItem key={item.avatar} item={item} />
+                        <OurHappyUser key={item.avatar} item={item} />
                     ))}
                     {foundersContent.map((item) => (
-                        <FoundersItem key={item.avatar} item={item} />
+                        <OurHappyUser key={item.avatar} item={item} />
                     ))}
                 </Slider>
             </div>
