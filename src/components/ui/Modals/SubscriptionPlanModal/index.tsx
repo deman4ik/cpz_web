8960e23004aa11924351dc61f8ee3994a4193175
Ilya Krupin <dev@krupibox.ru>
2021-02-18 20:20:19 +0300

    Fix page crashes

--- src/components/ui/Modals/SubscriptionPlanModal/index.tsx
+++ src/components/ui/Modals/SubscriptionPlanModal/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useMemo, useState, memo } from "react";
+import React, { useState, useEffect, memo } from "react";
 import { useMutation, useQuery } from "@apollo/client";
 import { SET_USER_SUB } from "graphql/profile/mutations";
 import { GET_SUBSCRIPTIONS } from "graphql/profile/queries";
@@ -20,74 +20,79 @@ interface Props {
 
 const _SubscriptionPlan: React.FC<Props> = ({ enabled, handleOnNext, handleOnClose, currentPlan }) => {
     const [formError, setFormError] = useState("");
-    const { data: dataSubs, loading: dataLoading } = useQuery(GET_SUBSCRIPTIONS);
-    const { subscriptions, subscription_options } = useMemo(() => (!dataLoading && dataSubs ? dataSubs : []), [
-        dataLoading,
-        dataSubs
-    ]);
+    const { data } = useQuery(GET_SUBSCRIPTIONS);
+    const [plan, setPlan] = useState<any>();
+    const [planActive, setPlanActive] = useState("");
+    const [periods, setPeriods] = useState([]);
+    const [subsPlan, setSubscriptionPlan] = useState({ name: "", description: "" });
+
+    useEffect(() => {
+        if (!data) return;
+
+        const { subscriptions, subscription_options } = data;
+        setSubscriptionPlan(subscriptions[0]);
+
+        const subsOptionsSorted = subscription_options.slice().sort((a, b) => a.sort_order - b.sort_order);
+        setPlan(subsOptionsSorted[1]);
+        setPeriods(subsOptionsSorted);
+
+        const [highlightedPlan] = subscription_options.filter((option) => option.highlight === true);
+        setPlanActive(highlightedPlan.name);
+    }, [data]);
 
     const [createUserSub] = useMutation(SET_USER_SUB);
 
-    const handleOnSubscription = ({ subscription_id, code }) => {
+    const handleOnSubscriptionPlan = ({ subscription_id, code }) => {
         createUserSub({
             variables: {
                 subscriptionId: subscription_id,
                 subscriptionOption: code
             }
         })
-            .then(() => {
+            .then(({ data: subscribedPlan }) => {
+                console.log(subscribedPlan);
                 if (handleOnClose) handleOnClose();
                 if (handleOnNext) setTimeout(() => handleOnNext(), 2000);
             })
             .catch(({ message }) => setFormError(message));
     };
 
-    const subsOptionsSorted =
-        subscription_options && subscription_options.slice().sort((a, b) => a.sort_order - b.sort_order);
-
-    const [subsActivePlan] =
-        subscription_options && subscription_options.filter((option) => option.name === currentPlan);
-
-    const [subsHighlightedPlan] =
-        subscription_options && subscription_options.filter((option) => option.highlight === true);
-
-    const [subName, setSubName] = useState(currentPlan || subsHighlightedPlan.name);
-    const [subPlan, setPlan] = useState(subsActivePlan);
     const handleOnButton = (card) => {
-        setSubName(card.name);
+        setPlanActive(card.name);
         setPlan(card);
     };
 
     return (
         <div className={styles.planContainer}>
-            <h3 className={styles.planName}>{subscriptions && subscriptions[0].name}</h3>
+            <h3 className={styles.planName}>{subsPlan.name}</h3>
             <div className={styles.buttonGroup}>
-                {subsOptionsSorted &&
-                    subsOptionsSorted.map((plan) => (
-                        <ButtonPrice
-                            key={plan.name}
-                            title={plan.name}
-                            classActive={plan.name === subName}
-                            plan={plan}
-                            handleOnButton={handleOnButton}
-                        />
-                    ))}
+                {periods.map((period) => (
+                    <ButtonPrice
+                        key={period.name}
+                        title={period.name}
+                        classActive={period.name === planActive}
+                        plan={period}
+                        handleOnButton={handleOnButton}
+                    />
+                ))}
             </div>
             <div className={styles.plan}>
-                {enabled && subsOptionsSorted && (
+                {enabled && subsPlan && (
                     <div className={styles.planCard}>
-                        <p className={styles.planDescription}>{subscriptions[0].description}</p>
-                        {subPlan && (
+                        <p className={styles.planDescription}>{subsPlan.description}</p>
+
+                        {plan && (
                             <>
                                 <b className={styles.planPrice}>
-                                    $ {getPriceTotalWithNoZero(subPlan.price_total)}&nbsp;
-                                    {subPlan.discount === null || `(-${subPlan.discount} %)`}
+                                    $ {getPriceTotalWithNoZero(plan.price_total)}&nbsp;
+                                    {plan.discount === null || `(-${plan.discount} %)`}
                                 </b>
                                 <p>
-                                    $ {subPlan.price_month} per {subPlan.unit}
+                                    $ {plan.price_month} per {plan.unit}
                                 </p>
                             </>
                         )}
+
                         <Button
                             buttonType={HTMLButtonTypes.button}
                             style={{ width: 140, margin: "0 auto" }}
@@ -96,8 +101,7 @@ const _SubscriptionPlan: React.FC<Props> = ({ enabled, handleOnNext, handleOnClo
                             size="small"
                             width={260}
                             isUppercase
-                            // disabled={status && subPlan.highlight}
-                            onClick={() => handleOnSubscription(subPlan)}
+                            onClick={() => handleOnSubscriptionPlan(plan)}
                         />
                     </div>
                 )}
